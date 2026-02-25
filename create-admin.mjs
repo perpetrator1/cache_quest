e0e258e97ba37a9ac1ab018aa23d7feaa823a1c2
Perpetrator <p3rp3trat0r1@gmail.com>
2026-02-25 10:52:24 +0530

    add dotenv package

--- create-admin.mjs
+++ create-admin.mjs
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+import { config } from 'dotenv';
+config();
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
